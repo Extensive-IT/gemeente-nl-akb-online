9d10c5f48ede107a6abeca0e2757d88dd23cbdfd
Jelle <jelle.aret@extensive-it.com>
2018-12-08 22:32:50 +0100

    Talen toegevoegd.

--- src/main/java/gemeente/nlakbonline/NlAkbOnlineApplication.java
+++ src/main/java/gemeente/nlakbonline/NlAkbOnlineApplication.java
@@ -2,13 +2,23 @@ package gemeente.nlakbonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-//@EnableOAuth2Client
 public class NlAkbOnlineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NlAkbOnlineApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		final SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(new Locale("nl", "NL"));
+		return slr;
 	}
 }
